79f7f0dcb66bc1f61dfa71b8739c439f07b8f996
Moises Icaza <icazamartinez@gmail.com>
2019-10-02 23:20:53 -0500

    Removed comments

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ const app = express();
 const host = 'localhost';
 const port = process.env.PORT || 10010;
 
-// API versioning instance
 const versioning = new ApiVersioning(__dirname);
-// Generate Swagger files for each API version
 const swaggerFile1 = ApiVersioning.generateSwaggerFile(host, port, './api/swagger/v1/swagger.yaml');
 const swaggerFile2 = ApiVersioning.generateSwaggerFile(host, port, './api/swagger/v2/swagger.yaml');
-// Make applications
+
 versioning.createAppVersion(app, swaggerFile1, '/api/v1', {
   swaggerUi: '/docs',
   apiDocs: '/swagger',
